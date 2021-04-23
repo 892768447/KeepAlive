c7e6b3543ac7768f41783cb2977d40d29803b561
Irony <892768447@qq.com>
2021-04-23 15:40:23 +0800

    test log

--- app/src/main/java/com/boolbird/keepalive/demo/Service1.java
+++ app/src/main/java/com/boolbird/keepalive/demo/Service1.java
@@ -5,29 +5,62 @@ import android.util.Log;
 
 import com.boolbird.keepalive.KeepAliveService;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Service1 extends KeepAliveService {
     private static final String TAG = "KeepAliveService1";
-    int x = 0;
-    private boolean exit = false;
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private final boolean exit = false;
+    private FileOutputStream fos;
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d(TAG, "Service1 started");
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate");
+        try {
+            fos = new FileOutputStream(getExternalFilesDir(null).getAbsolutePath() + "WhileSleep.log");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
         new Thread(new Runnable() {
             @Override
             public void run() {
                 while (!exit) {
                     try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
+                        Thread.sleep(3000);
+                        // 执行任务
+                        Log.i(TAG, "wait for 3s");
+                        if (fos != null) {
+                            fos.write(dateFormat.format(new Date()).getBytes());
+                            fos.write(" wait for 3s\n".getBytes());
+                            fos.flush();
+                        }
+                    } catch (InterruptedException | IOException e) {
                         e.printStackTrace();
                     }
-                    // int x = 8/0;
-                    x++;
-//                    Log.d("Service1", x + "");
                 }
             }
         }).start();
+        Log.d(TAG, "onCreate thread start");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "onStartCommand");
+        if (fos != null) {
+            try {
+                fos.write(dateFormat.format(new Date()).getBytes());
+                fos.write(" onStartCommand\n".getBytes());
+                fos.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 }
